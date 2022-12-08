ba36ff2b02982ee56e2543f607e9f61f5dc78856
Vamik76 <merts_v@mail.ru>
2022-12-08 19:57:09 +1000

    HW4C#_task1end

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+Console.WriteLine ("Введите первое чиcло");
+double a = Convert.ToDouble (Console.ReadLine ());
+Console.WriteLine ("Введите второе чиcло");
+double b = Convert.ToDouble (Console.ReadLine ());
+double c=Math.Pow(a,b);
+Console.WriteLine(c);
