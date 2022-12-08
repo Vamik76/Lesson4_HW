96b365502805eb486e5fa779b794430681180153
Vamik76 <merts_v@mail.ru>
2022-12-09 05:09:43 +1000

    HW4C#_task3end

--- Program.cs
+++ Program.cs
@@ -2,8 +2,12 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 Console.WriteLine ("Введите первое чиcло");
-double a = Convert.ToDouble (Console.ReadLine ());
+int a = Convert.ToInt32 (Console.ReadLine ());
 Console.WriteLine ("Введите второе чиcло");
-double b = Convert.ToDouble (Console.ReadLine ());
-double c=Math.Pow(a,b);
-Console.WriteLine(c);
+int b = Convert.ToInt32 (Console.ReadLine ());
+//double c=Math.Pow(a,b);
+//Console.WriteLine(c);
+int s=1;
+for ( int i=1;i<=b;i++)
+    s *= a;
+Console.WriteLine(s);
